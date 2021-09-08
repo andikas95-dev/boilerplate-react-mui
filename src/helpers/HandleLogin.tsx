0ac9904a1ce10883aaffc5f1b90696fb0467bfab
andikas95-dev <andikaperdana95@gmail.com>
2021-09-08 23:24:20 +0700

    refactor: :recycle: add handle login confirmation

---
+++ src/helpers/HandleLogin.tsx
@@ -0,0 +1,21 @@
+import { useLocalStorage } from 'react-use';
+
+const TOKEN_KEY = 'jwt';
+
+const [storageVal, setStorageVal, storageRemove] = useLocalStorage(TOKEN_KEY);
+
+export const login = (data: string) => {
+  setStorageVal(data);
+};
+
+export const logout = () => {
+  storageRemove();
+};
+
+export const isLogin = () => {
+  if (storageVal) {
+    return true;
+  }
+
+  return false;
+};
